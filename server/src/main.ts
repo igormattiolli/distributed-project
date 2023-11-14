9c483322f83b7e89aa6b4c6b69c457868a7d953e
IgorBMattiolli <igor.mattiolli@startse.com>
2023-11-13 22:20:04 -0300

    finish project

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,25 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from './common/exception-filters/all-exceptions.filter';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { AllExceptionsFilter } from "./common/exception-filters/all-exceptions.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: '*',
+    allowedHeaders: "*",
     origin: true,
-});
+  });
 
-app.useGlobalPipes(
+  app.useGlobalPipes(
     new ValidationPipe({
-        transform: true,
-        transformOptions: { enableImplicitConversion: true },
-        whitelist: true,
-        forbidNonWhitelisted: true,
-    }),
-);
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
 
-app.useGlobalFilters(new AllExceptionsFilter())
-  await app.listen(3000);
+  app.useGlobalFilters(new AllExceptionsFilter());
+  await app.listen(3333);
 }
 bootstrap();
--- server/src/product/product.gateway.ts
+++ server/src/product/product.gateway.ts
@@ -1,24 +1,23 @@
 import {
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
-} from '@nestjs/websockets';
-import { ProductCycleResponse } from './models/product.response';
-import { ProductStatus } from 'src/entities/Product.entity';
-import { Server } from 'socket.io';
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { ProductCycleResponse } from "./models/product.response";
+import { ProductStatus } from "src/entities/Product.entity";
+import { Server } from "socket.io";
 
-@WebSocketGateway(
-    {
-    cors: { origin: '*' },
-    }
-    )
+@WebSocketGateway({
+  cors: { origin: "*" },
+})
 export class ProductGateway {
-    @WebSocketServer()
-    private server: Server;
+  @WebSocketServer()
+  private server: Server;
 
-    changeProductStatus(products : Array<ProductCycleResponse>): Array<ProductCycleResponse> {
-        this.server.emit("status", products)
-        return products;
-    }
-
+  changeProductStatus(
+    products: Array<ProductCycleResponse>
+  ): Array<ProductCycleResponse> {
+    this.server.emit("status", products);
+    return products;
+  }
 }
